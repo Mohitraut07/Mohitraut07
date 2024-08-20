470c00c28207ac45b9689fed4affd3f230dee334
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-08-20 20:14:30 +0530

    chore: Comment out Feedbacks component in Home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -25,7 +25,7 @@ const Home = () => {
       <Experience />
       <Tech />
       <Works />
-      <Feedbacks />
+      {/* <Feedbacks /> */}
       <div className="relative z-0">
         <Contact />
         <StarsCanvas />
